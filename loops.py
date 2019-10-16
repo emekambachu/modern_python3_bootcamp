f439cb34ea2e3b24c8e25ab8d6fc4cc71d6a0081
chukwuemeka.mbachu <victor.c.mbachu@gmail.com>
2019-10-16 09:17:11 +0100

    working with iteration and functions

--- loops.py
+++ loops.py
@@ -2,5 +2,27 @@
 #     print(x)
 #     print(x*x)
 
-for x in "developer":
-    print(x)
+# for x in "developer":
+#     print(x)
+
+# list = ['eraser', 'pen', 'notebook']
+#
+# for x in list:
+#     if x == 'eraser':
+#         print(x + " is inside this list")
+
+
+dict = {
+    'chukwuma': 400000,
+    'pius': 700000,
+    'cathrine': 1000000
+}
+
+
+def pension(x):
+    p = (15/100) * x
+    return p
+
+for key, value in dict.items():
+    print(f"{key}'s pension amount is {pension(value)}")
+
